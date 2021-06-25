1688485fe00f7d1cc4588ccd66812f17b57c0175
Liftiz <marie.blanc.1996@gmail.com>
2021-06-25 19:51:39 +0200

    5ème commit

--- js/timer.js
+++ js/timer.js
@@ -4,7 +4,9 @@ var minutes = $( '#set-time' ).val();
 
 var target_date = new Date().getTime() + ((minutes * 60 ) * 1000); // set the countdown date
 var time_limit = ((minutes * 60 ) * 1000);
-//set actual timer
+//régler la minuterie actuelle
+
+
 setTimeout(
   function() 
   {
@@ -15,9 +17,9 @@ setTimeout(
     
   }, time_limit );
 
-var days, hours, minutes, seconds; // variables for time units
+var days, hours, minutes, seconds; // variables pour les unités de temps
 
-var countdown = document.getElementById("tiles"); // get tag element
+var countdown = document.getElementById("tiles"); // obtenir un élément de balise
 
 getCountdown();
 
@@ -25,7 +27,7 @@ var stop = setInterval(function () { getCountdown(); }, 1000);
 
 function getCountdown(){
 
-	// find the amount of "seconds" between now and target
+	// trouver le nombre de "secondes" entre maintenant et la cible
 	var current_date = new Date().getTime();
 	var seconds_left = (target_date - current_date) / 50;
   
@@ -50,7 +52,7 @@ if ( seconds_left >= 0 ) {
 	minutes = pad( parseInt(seconds_left / 60) );
 	seconds = pad( parseInt( seconds_left % 60 ) );
 
-	// format countdown string + set tag value
+	// format de la chaîne du compte à rebours + valeur de l'étiquette
 	countdown.innerHTML = "<span>" + hours + ":</span><span>" + minutes + ":</span><span>" + seconds + "</span>"; 
   
 
@@ -65,6 +67,7 @@ function pad(n) {
 	return (n < 10 ? '0' : '') + n;
 }
 
+// Bouton pour mettre en pause
 var p = document.getElementById("pause");
 var  paused = false;
 function stopCount(){
--- js/timer2.js
+++ js/timer2.js
@@ -4,7 +4,9 @@ var minutes = $( '#set-time' ).val();
 
 var target_date = new Date().getTime() + ((minutes * 60 ) * 1000); // set the countdown date
 var time_limit = ((minutes * 60 ) * 1000);
-//set actual timer
+//régler la minuterie actuelle
+
+
 setTimeout(
   function() 
   {
@@ -15,17 +17,17 @@ setTimeout(
     
   }, time_limit );
 
-var days, hours, minutes, seconds; // variables for time units
+var days, hours, minutes, seconds; // variables pour les unités de temps
 
-var countdown = document.getElementById("tiles"); // get tag element
+var countdown = document.getElementById("tiles"); // obtenir un élément de balise
 
 getCountdown();
 
-setInterval(function () { getCountdown(); }, 1000);
+var stop = setInterval(function () { getCountdown(); }, 1000);
 
 function getCountdown(){
 
-	// find the amount of "seconds" between now and target
+	// trouver le nombre de "secondes" entre maintenant et la cible
 	var current_date = new Date().getTime();
 	var seconds_left = (target_date - current_date) / 100;
   
@@ -50,7 +52,7 @@ if ( seconds_left >= 0 ) {
 	minutes = pad( parseInt(seconds_left / 60) );
 	seconds = pad( parseInt( seconds_left % 60 ) );
 
-	// format countdown string + set tag value
+	// format de la chaîne du compte à rebours + valeur de l'étiquette
 	countdown.innerHTML = "<span>" + hours + ":</span><span>" + minutes + ":</span><span>" + seconds + "</span>"; 
   
 
@@ -63,4 +65,15 @@ if ( seconds_left >= 0 ) {
 
 function pad(n) {
 	return (n < 10 ? '0' : '') + n;
+}
+
+// Bouton pour mettre en pause
+var p = document.getElementById("pause");
+var  paused = false;
+function stopCount(){
+	clearTimeout(stop);
+}
+
+function restart(){
+	setInterval(time_limit);
 }
